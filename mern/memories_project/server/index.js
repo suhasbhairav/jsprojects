77798b4c6b03d7dca821ea1d8d5f60f7684a7cfb
Suhas Bhairav <suhasbhairav@gmail.com>
2022-03-15 14:31:18 +0100

    Mern Stack - Social Media

---
+++ mern/memories_project/server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({
+    limit: "30mb",
+    extended: true
+}));
+
+app.use(bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true
+}));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://807774A29A610CFA5D8F44840650D00A27506F79:pbnoL8vPYrcrak67@cluster0.5gc9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));

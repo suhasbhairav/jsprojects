d48dcab5bd740394abadb99382e881ba88bee82d
Suhas Bhairav <suhasbhairav@gmail.com>
2022-03-16 15:10:57 +0100

    Add env

--- mern/memories_project/client/src/actions/posts.js
+++ mern/memories_project/client/src/actions/posts.js
@@ -1,21 +1,20 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
 import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
     try {
         const {data} = await api.fetchPosts();
-        dispatch({type: "FETCH_ALL", payload: data});
+        dispatch({type: FETCH_ALL, payload: data});
     
     } catch (error) {
         console.log(error.message);
-    }
-
-    
+    }    
 }
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const {data} = await api.createPost(post);
-        dispatch({type: 'CREATE', payload: data});
+        dispatch({type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -24,7 +23,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const {data} = await api.updatePost(id, post);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -33,7 +32,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async(dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch({type: "DELETE", payload: id});
+        dispatch({type: DELETE, payload: id});
     } catch (error) {
         console.log(error);
     }
@@ -42,7 +41,7 @@ export const deletePost = (id) => async(dispatch) => {
 export const likePost = (id) => async(dispatch) => {
     try {
         const {data} = await api.likePost(id);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: LIKE, payload: data});
     } catch (error) {
         console.log(error);
     }
--- mern/memories_project/server/index.js
+++ mern/memories_project/server/index.js
@@ -2,13 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-
-
+dotenv.config();
 
 app.use(bodyParser.json({
     limit: "30mb",
@@ -24,7 +22,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://807774A29A610CFA5D8F44840650D00A27506F79:pbnoL8vPYrcrak67@cluster0.5gc9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {

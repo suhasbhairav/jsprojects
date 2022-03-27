fcc5aaa9cc9a29435be906f4edb01abc5763b62f
Suhas Bhairav <suhasbhairav@gmail.com>
2022-03-27 10:44:49 +0200

    Update firebase-config.js

--- reactjs-firebase/reactjs-firebase-auth-project/src/firebase-config.js
+++ reactjs-firebase/reactjs-firebase-auth-project/src/firebase-config.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1CLFiDbT7LVDmQr6juoyt_FjcM0XOeYI",
-  authDomain: "reactsignup-9f1ff.firebaseapp.com",
-  projectId: "reactsignup-9f1ff",
-  storageBucket: "reactsignup-9f1ff.appspot.com",
-  messagingSenderId: "726277181506",
-  appId: "1:726277181506:web:c6d33b7503b0e30408dddb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase

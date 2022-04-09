cb49e303f65d540fd77bb3cc30e39990d782dd53
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-09 20:52:53 +0200

    TS Weekly Route

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
@@ -102,3 +102,24 @@ export const getTSWeeklyAdjusted = async (req, res) => {
       console.log(error.message);
   }
 };
+
+export const getTSWeekly = async (req, res) => {
+    const HOST = process.env.RAPID_API_HOST;
+    const KEY = process.env.RAPID_API_KEY;
+    try {
+       const options = {
+        method: 'GET',
+        url: 'https://'+HOST+'/query',
+        params: {function: 'TIME_SERIES_WEEKLY', symbol: 'MSFT', datatype: 'json'},
+        headers: {
+          'X-RapidAPI-Host': HOST,
+          'X-RapidAPI-Key': KEY
+        }
+       }; 
+
+       let response = await axios(options);
+       res.status(200).json(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+};

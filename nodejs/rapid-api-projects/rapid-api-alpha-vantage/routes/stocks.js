cd4d7d6d54f09645240bc1fbffc94b2a9fad5e7e
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-09 20:53:15 +0200

    Add TS Weekly Controller

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getTSIntraDay, getTSDailyAdjusted, getTSDaily, getTSWeeklyAdjusted} from '../controllers/stocks.js';
+import {getTSIntraDay, getTSDailyAdjusted, getTSDaily, getTSWeeklyAdjusted, getTSWeekly} from '../controllers/stocks.js';
 const router = express.Router();
 
 router.get('/tsIntra', getTSIntraDay);
 router.get('/tsDailyAdjusted', getTSDailyAdjusted);
 router.get('/tsDaily', getTSDaily);
 router.get('/tsWeeklyAdjusted', getTSWeeklyAdjusted);
+router.get('/tsWeekly', getTSWeekly);
 
 
 export default router;

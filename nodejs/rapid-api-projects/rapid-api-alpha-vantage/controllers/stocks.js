05df79fdf32b449eb54ebd1e24ba52df1afe5254
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-09 21:06:05 +0200

    Add TS Monthly

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
@@ -134,7 +134,7 @@ export const getTSMonthlyAdjusted = async (req, res) => {
   try {
     const options = {
       method: "GET",
-      url: "https://"+HOST+"/query",
+      url: "https://" + HOST + "/query",
       params: {
         symbol: "MSFT",
         function: "TIME_SERIES_MONTHLY_ADJUSTED",
@@ -150,5 +150,30 @@ export const getTSMonthlyAdjusted = async (req, res) => {
     res.status(200).json(response.data);
   } catch (error) {
     console.log(error.message);
+  }
+};
+
+export const getTSMonthly = async (req, res) => {
+  const HOST = process.env.RAPID_API_HOST;
+  const KEY = process.env.RAPID_API_KEY;
+  try {
+    const options = {
+      method: "GET",
+      url: "https://"+HOST+"/query",
+      params: {
+        symbol: "MSFT",
+        function: "TIME_SERIES_MONTHLY",
+        datatype: "json",
+      },
+      headers: {
+        "X-RapidAPI-Host": HOST,
+        "X-RapidAPI-Key": KEY,
+      },
+    };
+
+    let response = await axios(options);
+    res.status(200).json(response.data);
+  } catch (error) {
+      console.log(error.message);
   }
 };
--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
@@ -1,13 +1,21 @@
-import express from 'express';
-import {getTSIntraDay, getTSDailyAdjusted, getTSDaily, getTSWeeklyAdjusted, getTSWeekly, getTSMonthlyAdjusted} from '../controllers/stocks.js';
+import express from "express";
+import {
+  getTSIntraDay,
+  getTSDailyAdjusted,
+  getTSDaily,
+  getTSWeeklyAdjusted,
+  getTSWeekly,
+  getTSMonthlyAdjusted,
+  getTSMonthly,
+} from "../controllers/stocks.js";
 const router = express.Router();
 
-router.get('/tsIntra', getTSIntraDay);
-router.get('/tsDailyAdjusted', getTSDailyAdjusted);
-router.get('/tsDaily', getTSDaily);
-router.get('/tsWeeklyAdjusted', getTSWeeklyAdjusted);
-router.get('/tsWeekly', getTSWeekly);
-router.get('/tsMonthlyAdjusted', getTSMonthlyAdjusted);
-
+router.get("/tsIntra", getTSIntraDay);
+router.get("/tsDailyAdjusted", getTSDailyAdjusted);
+router.get("/tsDaily", getTSDaily);
+router.get("/tsWeeklyAdjusted", getTSWeeklyAdjusted);
+router.get("/tsWeekly", getTSWeekly);
+router.get("/tsMonthlyAdjusted", getTSMonthlyAdjusted);
+router.get("/tsMonthly", getTSMonthly);
 
 export default router;

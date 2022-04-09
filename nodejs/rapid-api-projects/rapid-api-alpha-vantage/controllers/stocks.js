eb89155ee7d7c9010d2995555f96ce3f373505b7
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-09 20:03:18 +0200

    Add Intra day Adjusted

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
@@ -23,6 +23,32 @@ export const getTSIntraDay = async (req, res) => {
     let response = await axios(options);
     res.status(200).json(response.data);
   } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getTSDailyAdjusted = async (req, res) => {
+  const HOST = process.env.RAPID_API_HOST;
+  const KEY = process.env.RAPID_API_KEY;
+  try {
+    const options = {
+      method: "GET",
+      url: "https://"+ HOST +"/query",
+      params: {
+        function: "TIME_SERIES_DAILY_ADJUSTED",
+        symbol: "MSFT",
+        outputsize: "compact",
+        datatype: "json",
+      },
+      headers: {
+        "X-RapidAPI-Host": HOST,
+        "X-RapidAPI-Key": KEY,
+      },
+    };
+
+    let response = await axios(options);
+    res.status(200).json(response.data);
+  } catch (error) {
       console.log(error.message);
   }
 };
--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/routes/stocks.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {getTSIntraDay} from '../controllers/stocks.js';
+import {getTSIntraDay, getTSDailyAdjusted} from '../controllers/stocks.js';
 const router = express.Router();
 
 router.get('/tsIntra', getTSIntraDay);
+router.get('/tsDailyAdjusted', getTSDailyAdjusted);
 
 export default router;

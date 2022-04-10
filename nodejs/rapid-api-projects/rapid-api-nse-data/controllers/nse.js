cc5aaee7f520a9e694a33e7b032d83fcf19feb2a
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-10 13:23:46 +0200

    Equity Stock Index

--- nodejs/rapid-api-projects/rapid-api-nse-data/controllers/nse.js
+++ nodejs/rapid-api-projects/rapid-api-nse-data/controllers/nse.js
@@ -29,10 +29,10 @@ export const get52WeekHigh = async(req, res) => {
     try {
         const options = {
             method: 'GET',
-            url: 'https://nse-data1.p.rapidapi.com/near_fifty_two_week_low',
+            url: 'https://'+HOST+'/near_fifty_two_week_low',
             headers: {
-              'X-RapidAPI-Host': 'nse-data1.p.rapidapi.com',
-              'X-RapidAPI-Key': '768a7a35f5mshbe332da2ffe63acp19162djsn465cb64e6088'
+              'X-RapidAPI-Host': HOST,
+              'X-RapidAPI-Key': KEY
             }
           };
 
@@ -41,5 +41,30 @@ export const get52WeekHigh = async(req, res) => {
     } catch (error) {
         console.log(error.message);
         res.status(400).json(error.message);
+    }
+};
+
+export const getEquityStockIndex = async (req, res) => {
+    const HOST = process.env.RAPID_API_HOST;
+    const KEY = process.env.RAPID_API_KEY;
+    const {symbol} = req.query;
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://'+ HOST +'/equity_stock_indices',
+            params: {
+                symbol: symbol
+            },
+            headers: {
+              'X-RapidAPI-Host': HOST,
+              'X-RapidAPI-Key': KEY
+            }
+          };
+
+          let response = await axios(options);
+          res.status(200).json(response.data);
+    } catch (error) {
+        console.log(error.message);
+        res.status(200).json(error.message);
     }
 };
--- nodejs/rapid-api-projects/rapid-api-nse-data/routes/nse.js
+++ nodejs/rapid-api-projects/rapid-api-nse-data/routes/nse.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {get52WeekHigh, get52WeekLow} from '../controllers/nse.js';
+import {get52WeekHigh, get52WeekLow, getEquityStockIndex} from '../controllers/nse.js';
 
 const app = express.Router();
 
 app.get('/yearlyWeekLow', get52WeekLow);
 app.get('/yearlyWeekHigh', get52WeekHigh);
+app.get('/equityStockIndex', getEquityStockIndex);
 
 export default app;

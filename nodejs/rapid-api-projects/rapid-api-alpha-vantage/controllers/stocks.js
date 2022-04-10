3fb0d11a92f20532d583ed4fc980abd78caae3e1
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-10 09:57:09 +0200

    Update stocks.js

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
@@ -129,6 +129,7 @@ export const getTSWeekly = async (req, res) => {
     res.status(200).json(response.data);
   } catch (error) {
     console.log(error.message);
+    res.status(400).json(error.message);
   }
 };
 

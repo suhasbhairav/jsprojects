ef7496019618004552e21e1ad33b34e1d202cf3b
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-10 09:57:57 +0200

    Update stocks.js

--- nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
+++ nodejs/rapid-api-projects/rapid-api-alpha-vantage/controllers/stocks.js
@@ -208,6 +208,7 @@ export const getFXMonthly = async (req, res) => {
     res.status(200).json(response.data);
   } catch (error) {
     console.log(error.message);
+    res.status(400).json(error.message);
   }
 };
 

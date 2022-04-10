806b3b2d8f934b39cd28e74d29454e1ac4a440cd
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-10 13:04:04 +0200

    NSE Weekly High

--- nodejs/rapid-api-projects/rapid-api-nse-data/controllers/nse.js
+++ nodejs/rapid-api-projects/rapid-api-nse-data/controllers/nse.js
@@ -21,3 +21,25 @@ export const get52WeekLow  = async (req, res) => {
         res.status(400).json(error.message);
     }
 };
+
+export const get52WeekHigh = async(req, res) => {
+    const HOST = process.env.RAPID_API_HOST;
+    const KEY = process.env.RAPID_API_KEY;
+    
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://nse-data1.p.rapidapi.com/near_fifty_two_week_low',
+            headers: {
+              'X-RapidAPI-Host': 'nse-data1.p.rapidapi.com',
+              'X-RapidAPI-Key': '768a7a35f5mshbe332da2ffe63acp19162djsn465cb64e6088'
+            }
+          };
+
+          let response = await axios(options);
+          res.status(200).json(response.data);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error.message);
+    }
+};
--- nodejs/rapid-api-projects/rapid-api-nse-data/routes/nse.js
+++ nodejs/rapid-api-projects/rapid-api-nse-data/routes/nse.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {get52WeekLow} from '../controllers/nse.js';
+import {get52WeekHigh, get52WeekLow} from '../controllers/nse.js';
 
 const app = express.Router();
 
 app.get('/yearlyWeekLow', get52WeekLow);
+app.get('/yearlyWeekHigh', get52WeekHigh);
 
 export default app;

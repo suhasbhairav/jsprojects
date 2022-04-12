3f59741bf75156951f7da2b54e2af03523acec4d
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-12 13:54:09 +0200

    Add Table for Alpha Vantage

--- reactjs-rapid-api/alpha-vantage-api/src/Dashboard.js
+++ reactjs-rapid-api/alpha-vantage-api/src/Dashboard.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [header, setHeader] = useState(null)
+
+  const createDataForTable = (col1, col2, col3, col4, col5, col6) => {
+      return {col1, col2, col3, col4, col5, col6};
+  }
 
   const getTSIntraday = async () => {
     const options = {
@@ -16,14 +28,35 @@ const Dashboard = () => {
         datatype: "json",
       },
       headers: {
-        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
-        "X-RapidAPI-Key": "768a7a35f5mshbe332da2ffe63acp19162djsn465cb64e6088",
+        "X-RapidAPI-Host": "",
+        "X-RapidAPI-Key": "",
       },
     };
 
     let response = await axios(options);
-    setData(response.data["Time Series (5min)"]);
-    console.log(response.data["Time Series (5min)"]);
+    let rows = Array()
+    let row = Array()
+    let headers = Array()
+    console.log(response.data);
+    //setData(response.data["Time Series (5min)"]);
+        let rowData = response.data["Time Series (5min)"]
+        /*Object.keys(rowData).map((header) => {
+            headers.push(header)
+        });*/
+
+        Object.keys(rowData).map((header) => {
+            //console.log(header);
+            rows.push(createDataForTable(
+                header,
+                rowData[header]["1. open"],
+                rowData[header]["2. high"],
+                rowData[header]["3. low"],
+                rowData[header]["4. close"],
+                rowData[header]["5. volume"]
+            ));     
+        });
+        console.log(rows);
+        setData(rows);
     
   };
 
@@ -33,13 +66,36 @@ const Dashboard = () => {
 
   return (
     <div>
-        {data && Object.keys(data).map((header) =>
-            <div>{header}: {
-                Object.keys(data[header]).map((subData) => 
-                    data[header][subData]
-                )
-            }</div>
-        )}
+        {data &&  <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Time</TableCell>  
+            <TableCell align="right">Open</TableCell>
+            <TableCell align="right">Close</TableCell>
+            <TableCell align="right">High</TableCell>
+            <TableCell align="right">Low</TableCell>
+            <TableCell align="right">Volume</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              
+              <TableCell align="right">{row.col1}</TableCell>
+              <TableCell align="right">{row.col2}</TableCell>
+              <TableCell align="right">{row.col3}</TableCell>
+              <TableCell align="right">{row.col4}</TableCell>
+              <TableCell align="right">{row.col5}</TableCell>
+              <TableCell align="right">{row.col6}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>}
     </div>
   );
 };

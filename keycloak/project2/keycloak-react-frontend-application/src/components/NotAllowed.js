92f31561bbf4b44d3efe9fed86b837aac9b05636
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-14 15:16:45 +0200

    Add Roles

---
+++ keycloak/project2/keycloak-react-frontend-application/src/components/NotAllowed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotAllowed = () => {
+  return (
+    <div>NotAllowed</div>
+  )
+}
+
+export default NotAllowed
--- keycloak/project2/keycloak-react-frontend-application/src/components/RolesRoute.js
+++ keycloak/project2/keycloak-react-frontend-application/src/components/RolesRoute.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import NotAllowed from '../../../react-frontend/src/components/NotAllowed'
+import UserService from '../../../react-frontend/src/services/UserService'
+import {Route} from 'react-router-dom'
 
-const RolesRoute = () => {
-  return (
-    <div>RolesRoute</div>
-  )
-}
+const RolesRoute = ({roles, children, ...rest}) => (
+  <Route {...rest}>
+    {UserService.hasRole(roles) ? children : <NotAllowed />}
+  </Route>
+)
 
 export default RolesRoute

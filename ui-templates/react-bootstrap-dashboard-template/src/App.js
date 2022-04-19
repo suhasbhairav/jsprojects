79a52dd84cc03d9b233f7e142522a5d0c4dc0529
Suhas Bhairav <suhasbhairav@gmail.com>
2022-04-19 13:36:32 +0200

    UI Templates-Bootstrap and React Init

---
+++ ui-templates/react-bootstrap-dashboard-template/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBarComponent from './NavBarComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ ui-templates/react-bootstrap-dashboard-template/src/NavBarComponent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Container, Nav, NavLink, NavbarBrand, NavDropdown } from "react-bootstrap";
+
+const NavBarComponent = () => {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React Bootstrap Dashboard</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Contact Us</Nav.Link>
+              <NavDropdown title="UI Template Design Team" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Team
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Who are we
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBarComponent;
